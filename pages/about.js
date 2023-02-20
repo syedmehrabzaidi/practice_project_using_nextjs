c0e858f4fe4410c0cd782b542a7a2e923a4d072d
unknown <syedmehrab2288@gmail.com>
2023-02-21 01:09:19 +0500

    fetch_data | integrate apis and added fancy button

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,14 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Title from '../conponents/Title';
+
 import styles from '../styles/Home.module.css';
 
 export default function About() {
   return (
     <div>
+            <Title title="about"></Title>
+
    <h1>About US</h1>
    <ul>
    <li><Link href="/">Home</Link></li>
